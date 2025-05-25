97022c08d3b301c193b7d57afc79a18bd51c088f
syedtalhaaskari <syedtalhaaskari@gmail.com>
2025-05-25 15:33:36 +0500

    Complete Exercise 2

--- app.py
+++ app.py
@@ -7,4 +7,18 @@ def fn1(func2):
     print("I am function 1")
     func2()
 
-fn1(fn2)
+# fn1(fn2)
+
+# Exercise 2
+def auth_checker(func1):
+    def wrapper():
+        print("Auth Checker Called")
+        func1()
+    
+    return wrapper
+
+@auth_checker
+def access_dashboard():
+    print("Access Dashboard Called")
+
+access_dashboard("Talha")
