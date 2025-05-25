49635d5ee7f5b6b3e32c5425c977737ed58c349d
syedtalhaaskari <syedtalhaaskari@gmail.com>
2025-05-25 18:04:58 +0500

    Complete Exercise 7

--- app.py
+++ app.py
@@ -55,5 +55,29 @@ names = ["Hamzah", "Ali", "Arham"]
 #     print(i)
 
 # Exercise 6
-for i in range(1, 11):
-    print(f"2 * {i} = {2 * i}")
+# for i in range(1, 11):
+#     print(f"2 * {i} = {2 * i}")
+
+# Exercise 7
+import random
+
+secret_number = random.randint(1, 100)
+
+while True:
+    my_guess = int(input("\nGuess the number: "))
+
+    if my_guess > secret_number + 20:
+        print("You Guessed Too High!")
+    elif my_guess > secret_number + 10:
+        print("You Guessed High!")
+    elif my_guess > secret_number:
+        print("You Guessed Little Bit High!")
+    elif my_guess + 20 < secret_number:
+        print("You Guessed Too Low!")
+    elif my_guess + 10 < secret_number:
+        print("You Guessed Low!")
+    elif my_guess < secret_number:
+        print("You Guessed Little Bit Low!")
+    else:
+        print("You Won!\n")
+        break
