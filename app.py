e0a3108f8e182b303a30e9263e8b29ccb186eaa5
syedtalhaaskari <syedtalhaaskari@gmail.com>
2025-05-25 16:18:42 +0500

    Complete Exercise 3

--- app.py
+++ app.py
@@ -10,15 +10,36 @@ def fn1(func2):
 # fn1(fn2)
 
 # Exercise 2
-def auth_checker(func1):
-    def wrapper():
-        print("Auth Checker Called")
-        func1()
+# def auth_checker(func1):
+#     def wrapper():
+#         print("Auth Checker Called")
+#         func1()
     
-    return wrapper
+#     return wrapper
 
-@auth_checker
+# @auth_checker
+# def access_dashboard():
+#     print("Access Dashboard Called")
+
+# access_dashboard("Talha")
+
+# Exercise 3
+def auth_checker_with_parameters(name, password):
+    def auth_checker(func1):
+        def wrapper():
+            print("Auth Checker Called")
+            if name == "Talha" and password == "Password":
+                return func1()
+            else:
+                print("Invalid")
+        return wrapper
+        
+    return auth_checker
+
+username = "Talha"
+pwd = "Password"
+@auth_checker_with_parameters(username, pwd)
 def access_dashboard():
-    print("Access Dashboard Called")
+    print("Access Dashboard Called ")
 
-access_dashboard("Talha")
+access_dashboard()
